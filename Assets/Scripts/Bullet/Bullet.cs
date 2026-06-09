3a277095c3964c6582f3cd9754d41180d2d8c4db
FanJae <hjjang45@gmail.com>
2026-06-09 18:42:18 +0900

    feat : 플레이어 총알 발사 로직 구현
    - 플레이어의 총알 발사 로직을 구현했습니다.

---
+++ Assets/Scripts/Bullet/Bullet.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 10.0f;
+    [SerializeField] private float lifeTime = 3f;
+    [SerializeField] private int damage = 1;
+
+    public int Damage => damage; // => 읽기 전용 프로퍼티.
+    void Start()
+    {
+        Destroy(gameObject, lifeTime);
+    }
+    void Update()
+    {
+        transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
+    }
+}
