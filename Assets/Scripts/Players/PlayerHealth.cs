2f018bbb7ad37b14477b3f7fcf2233e8a5b609be
FanJae <hjjang45@gmail.com>
2026-06-09 18:39:34 +0900

    feat : 플레이어의 Life UI 구현
    - 플레이어의 Life가 갱신되도록 구현했습니다.

---
+++ Assets/Scripts/Players/PlayerHealth.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] private GameManager gameManager;
+    [SerializeField] private Image[] lifeImage;
+    [SerializeField] private int maxHp = 3;
+    private int currentHp;
+
+    public bool IsDead => currentHp <= 0; 
+    void Start()
+    {
+        currentHp = maxHp;
+    }
+    public void TakeDamage(int damage)
+    {
+        if (IsDead) return;
+
+        currentHp -= damage;
+        
+        UpdateUI();
+        if(currentHp <= 0)
+        {
+            gameManager.GameOver();
+        }
+    }
+
+    private void UpdateUI()
+    {
+        for(int i = 0; i < lifeImage.Length; i++)
+        {
+            if (i < currentHp)
+            {
+                lifeImage[i].enabled = true;
+            }
+            else
+            {
+                lifeImage[i].enabled = false;
+            }
+        }
+    }
+}
