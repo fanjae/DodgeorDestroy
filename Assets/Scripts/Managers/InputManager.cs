8abbb10012b27bd59731d67d9c7d53a5f4fc7c27
FanJae <hjjang45@gmail.com>
2026-06-09 18:40:53 +0900

    feat : 플레이어 입력 처리 매니저 구현
    - 플레이어 입력을 처리하는 매니저를 구현하였습니다.

---
+++ Assets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    // 플레이어 이동 입력값
+    public static Vector2 Movement { get; private set; } = Vector2.zero;
+
+    // 공격 버튼 입력 여부
+    public static bool IsFire { get; private set; } = false;
+
+    private InputAction moveAction;
+    private InputAction fireAction;
+
+    private void Awake()
+    {
+        // Input System의 액션 가져오기
+        moveAction = InputSystem.actions.FindAction("Move");
+        fireAction = InputSystem.actions.FindAction("Attack");
+    }
+    void Update()
+    {
+        Movement = moveAction.ReadValue<Vector2>();
+        IsFire = fireAction.WasPressedThisFrame();
+    }
+}
