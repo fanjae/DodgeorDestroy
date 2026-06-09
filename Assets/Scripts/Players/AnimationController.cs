e09a198f122e6b73c0dbe08cd3b30a30ef19192a
FanJae <hjjang45@gmail.com>
2026-06-09 18:40:12 +0900

    feat : 플레이어 애니메이션 상태 전달 로직 구현
    - 플레이어 애니메이션의 상태 전달 로직을 구현했습니다.

---
+++ Assets/Scripts/Players/AnimationController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    private static readonly int movehash = Animator.StringToHash("Move");
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponentInParent<Animator>();
+    }
+
+    public void SetMoveState(int moveState)
+    {
+        animator.SetInteger(movehash, moveState);
+    }
+
+    void Update()
+    {
+        
+    }
+}
