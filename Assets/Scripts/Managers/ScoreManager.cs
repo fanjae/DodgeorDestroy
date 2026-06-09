2fbc30ed0fe09d5265c0645564381eb24c85ddfe
FanJae <hjjang45@gmail.com>
2026-06-09 18:40:38 +0900

    feat : 점수 관리 매니저 구현
    - 적을 잡으면 점수를 주는 부분을 처리하는 Score Manager를 구현했습니다.

---
+++ Assets/Scripts/Managers/ScoreManager.cs
@@ -0,0 +1,27 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    [SerializeField] private TMP_Text scoreText; // 점수를 표시할 UI 텍스트
+
+    private int score = 0;
+    public int Score => score;
+
+    private void Start()
+    {
+        // 시작 시 초기 점수 UI 표시
+        UpdateScoreUI();
+    }
+
+    public void AddScore(int amount)
+    {
+        score += amount;
+        UpdateScoreUI();
+    }
+
+    private void UpdateScoreUI() // 점수 갱신
+    {
+        scoreText.text = "Score : " + score;
+    }
+}
