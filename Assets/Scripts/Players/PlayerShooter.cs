a6d02266f264f1225c483fc7a7692d1540869d1b
FanJae <hjjang45@gmail.com>
2026-06-09 18:38:50 +0900

    feat : 플레이어 발사 로직 구현
    - 플레이어가 총알을 발사하는 로직을 구현했습니다.

---
+++ Assets/Scripts/Players/PlayerShooter.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class PlayerShooter : MonoBehaviour
+{
+    [Header("발사 위치")]
+    [SerializeField] private Transform firePoint;
+    [Header("총알 프리팹")]
+    [SerializeField] private GameObject bulletPrefab;
+
+    [SerializeField] private float fireDelay = 0.2f;
+    private float fireTimer = 0.0f;
+
+    private void Update()
+    {
+        // 경과시간 누적
+        fireTimer += Time.deltaTime;
+
+        if(InputManager.IsFire)
+        {
+            // 발사
+            Fire();
+        }
+    }
+    private void Fire()
+    {
+        // 발사 대기 시간이 지나지 않았으면 하지 말자
+        if(fireTimer < fireDelay)
+        {
+            return;
+        }
+        fireTimer = 0.0f;
+
+        Instantiate(bulletPrefab, firePoint.position, Quaternion.identity);
+    }
+}
