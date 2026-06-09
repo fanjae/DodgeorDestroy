134e49a69acde79e7adb29798330397ebfcf06f4
FanJae <hjjang45@gmail.com>
2026-06-09 18:42:36 +0900

    feat : 게임 매니저 로직 구현
    - GameOver 관련 처리를 담당하는 게임 매니저 로직을 구현했습니다.

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private GameObject gameOverPanel;
+    [SerializeField] private TMP_Text gameOverScoreText;
+    [SerializeField] private ScoreManager scoreManager;
+
+    private bool isGameOver = false;
+
+    private void Start()
+    {
+        // 게임 오버 패널 숨김
+        gameOverPanel.SetActive(false);
+    }
+
+    private void Update()
+    {
+        // 게임 오버 상태에서 아무키나 누르면 종료 시킴
+        if (isGameOver && Keyboard.current != null && Keyboard.current.anyKey.wasPressedThisFrame)
+        {
+            #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+            #else
+                Application.Quit();
+            #endif
+        }
+    }
+
+    public void GameOver()
+    {
+        // 게임 오버 된 경우 중복 실행 방지
+        if (isGameOver) return;
+
+        isGameOver = true;
+        gameOverPanel.SetActive(true);
+
+        gameOverScoreText.text = "Your Score : " + scoreManager.Score;
+
+        // 게임 정지
+        Time.timeScale = 0f;
+    }
+}
