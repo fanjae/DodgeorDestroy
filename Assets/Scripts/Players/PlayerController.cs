7f371604109d99be19f7485b2059229e5a95c1b9
FanJae <hjjang45@gmail.com>
2026-06-09 18:39:54 +0900

    feat : 플레이어의 이동 구현
    - 플레이어의 이동 관련된 로직을 구현하고, 애니메이션 상태를 전달하도록 처리했습니다.

---
+++ Assets/Scripts/Players/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5.0f;
+    [SerializeField] AnimationController animationController;
+
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        moveInput = InputManager.Movement;
+        // 애니메이션 상태 갱신
+        UpdateAnimation();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = moveInput * moveSpeed;
+    }
+
+    private void UpdateAnimation()
+    {
+        int moveState = 0;
+        if (moveInput.x > 0.01f) // 오른쪽
+        {
+            moveState = 1;
+        }
+        else if(moveInput.x < -0.01f) // 왼쪽
+        {
+            moveState = -1;
+        }
+        // 애니메이터 상태 전달
+        animationController.SetMoveState(moveState);
+    }
+}
