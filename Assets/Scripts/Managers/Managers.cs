7ef66b7aaa92757ee973d0e0e41a29aebd71628f
FanJae <hjjang45@gmail.com>
2026-06-10 18:37:14 +0900

    feat : 매니저 추가
    - PoolManager에 접근시 오브젝트를 생성하도록 하는 Managers 추가

---
+++ Assets/Scripts/Managers/Managers.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+// 해당 클래스는 여러 매니저에 쉽게 접근하기 위한 클래스
+public static class Managers
+{
+    private static PoolManager _pool;
+
+    public static PoolManager Pool
+    {
+        get
+        {
+            if(_pool == null)
+            {
+                GameObject obj = new GameObject("PoolManager");
+                _pool = obj.AddComponent<PoolManager>();
+                Object.DontDestroyOnLoad(obj);
+            }
+            return _pool;
+        }
+    }
+}
